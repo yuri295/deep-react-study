cfc9da8fd79f31eaca757a6d3b7189b001ddd8d0
yuri295 <jin_astro0302@naver.com>
2024-01-29 19:23:54 +0900

    react component

---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,15 @@
+const MyComponent = (props) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {props.name}입니다.
+      <br />
+      children값은 {props.children} 입니다.
+    </div>
+  );
+};
+
+MyComponent.defaultProps = {
+  name: '기본 이름',
+};
+
+export default MyComponent;
