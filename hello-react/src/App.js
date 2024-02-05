5e9d0330234a218db70f772a8083e5b9c7b2e9da
yuri295 <jin_astro0302@naver.com>
2024-02-05 19:42:22 +0900

    feat: Component ref

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,9 +2,20 @@ import MyComponent from './MyComponent';
 import Say from './say';
 import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
+import { Component } from 'react';
 
-const App = () => {
-  return <ValidationSample />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
