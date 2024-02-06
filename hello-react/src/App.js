2a3f89d5bff575d9fe175fe31c043d56a7249174
yuri295 <jin_astro0302@naver.com>
2024-02-06 17:29:41 +0900

    feat: map

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,17 +4,11 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import { Component } from 'react';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
